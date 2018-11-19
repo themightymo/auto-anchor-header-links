576d8bbea1f9c97a38325d9779bb55a1ca0097f2
Toby Cryns <toby@themightymo.com>
2018-11-19 10:27:18 -0600

    update fontawesome class

--- auto-header-anchor-links.php
+++ auto-header-anchor-links.php
@@ -37,7 +37,7 @@ function auto_id_headings( $content ) {
 
 	$content = preg_replace_callback( '/(\<h[1-6](.*?))\>(.*)(<\/h[1-6]>)/i', function( $matches ) {
 		if ( ! stripos( $matches[0], 'id=' ) ) :
-			$heading_link = '<a href="#' . sanitize_title( $matches[3] ) . '" class="heading-link"><i class="fas fa-link"></i></a>';
+			$heading_link = '<a href="#' . sanitize_title( $matches[3] ) . '" class="heading-link"><i class="fa fa-link"></i></a>';
 			$matches[0] = $matches[1] . $matches[2] . ' id="' . sanitize_title( $matches[3] ) . '">' . $heading_link . $matches[3] . $matches[4];
 		endif;
 
